0f6f26e701fb642787dbdc4c7d2af0eaeb1eb359
Sias <15emiliomurray@gmail.com>
2025-01-16 09:55:15 +0200

    Set up automatic shortcut creation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from multiprocessing.spawn import freeze_support
 
 import requests
 import FreeSimpleGUI as sg
+import time
+
+
+
 
 def main():
     api_key = "1qT5nfSAXcr7VcrlhrzdtXw3YLReTC049uGA1Elo"
@@ -18,8 +22,10 @@ def main():
 
     explain = sg.Text("Choose the filepath where you want to download the NASA daily images to.")
     filepath_input = sg.Input("Filepath Here",key= "Path")
+    check_box = sg.Checkbox("Would you like this to automatically run on startup?",key="check")
     button = sg.Button("DONE",key="Done")
     layout = [[explain],
+              [check_box],
               [filepath_input,button]]
 
     window = sg.Window("wallpaper Downloader", layout=layout)
@@ -35,12 +41,35 @@ def main():
                     images_path = values["Path"]
                     with open("filepath.txt","w") as file:
                         file.write(images_path)
-                    break
 
+                    if values['check']:
+                        make_shortcut()
+                    break
 
     with open(images_path+image_name,"wb") as file:
         file.write(image_bits.content)
+    window.close()
+
+def make_shortcut():
+    second_part = r"AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+    import os,winshell
+    from win32com.client import Dispatch
+
+
+    username = winshell.desktop()[:-7]
+    w_dir = fr"{username}{second_part}"
+    path = fr"{w_dir}\main.lnk"
+    target = fr"{os.path.abspath(os.path.dirname(__file__))}\main.exe"
+
+
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = w_dir
+    shortcut.save()
+
 
 if __name__ == '__main__':
     freeze_support()
     main()
+    time.sleep(300)
