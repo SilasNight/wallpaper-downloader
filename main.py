be06fd82d24ef367c91bcbaa9863e6b6f19c7eac
Sias <15emiliomurray@gmail.com>
2025-01-20 15:04:23 +0200

    checks the image before downloading it and saves the text file to the documents folder

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from multiprocessing.spawn import freeze_support
 
-import requests
+import requests,winshell
 import FreeSimpleGUI as sg
 import time
 
-
+username = winshell.desktop()[:-7]
+text_file_name = "Wallpaper Downloader.txt"
+filepath = fr"{username}\Documents\{text_file_name}"
 
 
 def main():
@@ -17,7 +19,7 @@ def main():
 
 
     image_url = content["hdurl"]
-    image_bits = requests.get(image_url)
+
     image_name = "\\"+content["date"] + ".jpg"
 
     explain = sg.Text("Choose the filepath where you want to download the NASA daily images to.")
@@ -32,55 +34,68 @@ def main():
 
     window = sg.Window("wallpaper Downloader", layout=layout)
     try:
-        with open("filepath.txt", "r") as file:
+        with open(filepath, "r") as file:
             images_path = file.read()
     except FileNotFoundError:
         while True:
             event, values = window.read()
             match event:
                 case "Done":
-                    print("great")
-                    images_path = values["Path"]
-                    with open("filepath.txt","w") as file:
-                        file.write(images_path)
 
-                    if values['check']:
-                        make_shortcut()
-                    break
+                    images_path = values["Path"]
+                    if check_path(images_path):
+                        window['Path'].update("Seems the filepath might not be right please check it.")
+                    else:
+                        with open(filepath,"w") as file:
+                            file.write(images_path)
+
+                        if values['check']:
+                            make_shortcut()
+                        break
                 case "file":
                     images_path = values["file"]
                     window['Path'].update(images_path)
                 case sg.WIN_CLOSED:
-                    window.close()
+                    break
+        window.close()
     try:
 
         with open(images_path + image_name, "rb") as file:
             file.read()
-        print("was there")
+
     except FileNotFoundError:
-        print("wasn't there")
+        image_bits = requests.get(image_url)
         with open(images_path+image_name,"wb") as file:
             file.write(image_bits.content)
     window.close()
 
+
 def make_shortcut():
     second_part = r"AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-    import os,winshell
+    import os
     from win32com.client import Dispatch
 
 
-    username = winshell.desktop()[:-7]
+
     w_dir = fr"{username}{second_part}"
-    path = fr"{w_dir}\main.lnk"
+    path = fr"{w_dir}\main.exe - Shortcut.lnk"
     target = fr"{os.path.abspath(os.path.dirname(__file__))}\main.exe"
+
 
 
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(path)
-    shortcut.Targetpath = target
+    shortcut.TargetPath = target
     shortcut.WorkingDirectory = w_dir
     shortcut.save()
 
+
+def check_path(filepath):
+    items_path = filepath.split('/')
+    if ":" in items_path[0]:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     freeze_support()
